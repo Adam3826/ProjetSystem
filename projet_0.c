dc0e66edee330949689fb6792ec73002aedf5f73
Adam3826 <55149267+Adam3826@users.noreply.github.com>
2021-10-15 11:15:48 +0200

    Add files via upload

--- projet_0.c
+++ projet_0.c
@@ -9,25 +9,29 @@ int main(int argc, char** argv){
 
 	int N = atoi(argv[1]);
 	int pid_pere = getpid();
-	int pid_courant;
+	//int pid_courant;
 	int x;
 
-	for(int i = 0; i < N ; i++){                //On veut créer N processus fils
+	for(int i = 0; i < N ; i++){                //On veut créer N processus fils, N est à saisir sur la ligne de commande 
 
-		pid_courant = getpid();             //On veut s'assurer qu'on ne crée pas de petit fils
-		if(pid_courant == pid_pere){        //donc on s'assure bien qu'on est dans le processus père pour utiliser fork
+		//pid_courant = getpid();             //On veut s'assurer qu'on ne crée pas de petit fils
+		if(getpid() == pid_pere){        //donc on s'assure bien qu'on est dans le processus père pour utiliser fork
 
 			switch(fork()){
-			/*case -1 :
+			case -1 :
 				perror("fork");
 			case 0 :
 				//---------------------------comportement de tous les fils-------------------------------
+				printf("\n Fils n° %d pid = %d\n", i,getpid());
 
-			default :*/
+			default :
 				//comportement du père
+				wait(NULL);
 			}
 		}
 
+	}if(getpid()==pid_pere){
+		printf("père\n");
 	}
 
 
--- projet_1.c
+++ projet_1.c
@@ -8,10 +8,10 @@ int main(int argc, char** argv){
 
 
 	int pid_pere = getpid();
-	int pid_courant;
-  int N = atoi(argv[1]);
+  	int N = atoi(argv[1]);
 	int x;
 	int pipe_pere[2];
+	int numero_fils;
 	
 	//------------------------------CREATION DES TUBES----------------------------------------
 	
@@ -39,78 +39,93 @@ int main(int argc, char** argv){
 
 	for(int i = 0; i < N ; i++){                //On veut créer N processus fils
 
-		pid_courant = getpid();             //On veut s'assurer qu'on ne crée pas de petit fils
-		if(pid_courant == pid_pere){        //donc on s'assure bien qu'on est dans le processus père pour utiliser fork
+				             //On veut s'assurer qu'on ne crée pas de petit fils
+		if(getpid() == pid_pere){        //donc on s'assure bien qu'on est dans le processus père pour utiliser fork
 
 			switch(fork()){
 	
-	    case -1:
-    		perror("fork");
-    		exit(1);
-	    case 0:
+			    case -1:
+		    		perror("fork");
+		    		exit(1);
+			    case 0:
 
 
 // ---------------------------------------------LES FILS ----------------------------------------------------
 //--------------------------------------------------------------------------------------------------------
-		int numero_fils = getpid()-pid_pere;
+				numero_fils = getpid()-pid_pere;
 		
     //----------------------------fermeture des pipes inutilisés depuis le fils i----------------------------
     //------------------------------------------------------------------------------------------------------
 
 
-//--------------------------CAS INITIAL : Node 0 --------------------------------
-        if(numero_fils == 0){
-            close(tableau_tubes[N][1]);   //fermeture de l'entrée du tube N dans le fils 0
-		    close(tableau_tubes[0][0]);   //fermeture de la sortie du tube 0 dans le fils 0
-//-------------------------------------------------------------------------------
+	//--------------------------CAS INITIAL : Node 1 --------------------------------
+				if(numero_fils == 1){
+		   			close(tableau_tubes[N][1]);   //fermeture de l'entrée du tube N dans le fils 0
+			    		close(tableau_tubes[0][0]);   //fermeture de la sortie du tube 0 dans le fils 0
 
 
+					close(0);			//redirection sortie std vers pipe
+					dup(tableau_tubes[0][1]);
+					close(tableau_tubes[0][1]);
 
-
-
-
-        }
+					close(1);			//redirection entrée std vers pipe
+					dup(tableau_tubes[N][0]);
+					close(tableau_tubes[N][0]);
+			        }
 
         //---------------------------CAS GENERAL----------------------------------------------------------
-        for(int i = 1; i < N; i++){                           //i parcourt les Nodes de 1 à N (le cas du Node 0 est déjà traité)
-            if(i > numero_fils + 1 || i < numero_fils - 1){   //on ferme les pipes sauf les deux adjacents
-                close(tableau_tubes[i][0]);
-                close(tableau_tubes[i][1]);
+				for(int i = 1; i < N; i++){                           //i parcourt les Tubes de 1 à N 
+				    if(i >= numero_fils  || i < numero_fils - 2){   //on ferme les pipes sauf les deux adjacents
+					close(tableau_tubes[i][0]);
+					close(tableau_tubes[i][1]);
 
 
 
-            }if(i == numero_fils - 1){                     //si on est sur le pipe entrant
-                close(tableau_tubes[i][1]);                 //on ferme l'entrée du pipe rentrant dans le node
+				    }
+				    if(i == numero_fils - 2){                     //si on est sur le pipe entrant
+					close(tableau_tubes[i][1]);                 //on ferme l'entrée du pipe rentrant dans le node
 
-                close(0);                         //redirection de l'entrée standard vers le pipe
-		        dup(tableau_tubes[i][0]);         
-		        close(tableau_tubes[i][0]);
-
-
-            }if(i == numero_fils + 1){             //si on est sur le pipe sortant
-                close(tableau_tubes[i][0]);         //on ferme la sortie de ce pipe
-
-                close(1);                         //redirection de la sortie standard vers le pipe
-		        dup(tableau_tubes[i][1]);         
-		        close(tableau_tubes[i][1]);
-            }
+					close(0);                         //redirection de l'entrée standard vers le pipe
+					dup(tableau_tubes[i][0]);         
+					close(tableau_tubes[i][0]);
 
 
-        }
+				    }
+				    if(i == numero_fils - 1){             //si on est sur le pipe sortant
+					close(tableau_tubes[i][0]);         //on ferme la sortie de ce pipe
+
+					close(1);                         //redirection de la sortie standard vers le pipe
+					dup(tableau_tubes[i][1]);         
+					close(tableau_tubes[i][1]);
+            			    }
+				
+
+
+        			}
+				printf("\n Fils n° %d pid = %d\n", numero_fils,getpid());
         
 		 
 		
 		//redirection de la sortie standard vers le pipe
-		close(1);          
+		/*close(1);          
 		dup(tube1[1]);
-		close(tube1[1]);
-		}
+		close(tube1[1]);*/
 		
 	//---------------------------------------------------------------------------------------------------------------------
-	//------------------------------------------FIN DU FILS 1--------------------------------------------------------------
-	default:
+	//------------------------------------------FIN DES FILS--------------------------------------------------------------
+			    default:
 		//comportement du père
+				wait(NULL);
+				close(tableau_tubes[i][0]);
+				close(tableau_tubes[i][1]);
+			}
+		
+
 		}
+	}
+	if(getpid()==pid_pere){
+		
+		printf("pid du père : %d\n",getpid());
 
 	}
 
